69da4126ba09e8bf08305c6c903c4f0bcfa83131
JoaoVitorBranco <jvtchbr@gmail.com>
2024-04-27 10:52:00 -0300

    refat(lembrete): adiciona variavel PORT

--- python/main.py
+++ python/main.py
@@ -47,4 +47,4 @@ def delete_item(id_lembrete: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=PORT)
